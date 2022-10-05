cc8d11eb129781b48851b558f6feafa53eb92a52
Leoš Gjumija <gjumle@protonmail.com>
2022-10-05 23:42:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,25 @@ $conn = db_conn('localhost', 'lastSeenAdmin', 'lsa', 'lastSeen', TRUE);
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Index</title>
-    </head>
-    <body>
+        <title>lastSeen</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script>
             console.log("<?php echo $conn; ?>");
             alert("<?php echo $conn; ?>");
         </script>
+        <link rel="stylesheet" href="css/master.css">
+        <!-- FONTS -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+       <div class="nav_bar">
+              <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+              </ul>
+       </div>
     </body>
