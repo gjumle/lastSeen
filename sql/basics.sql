4d25955d06635d230ea21a16e5dd9482e9924757
Leoš Gjumija <gjumle@protonmail.com>
2022-10-14 11:44:38 +0200

    Update basics.sql

--- sql/basics.sql
+++ sql/basics.sql
@@ -14,11 +14,9 @@ CREATE TABLE IF NOT EXISTS 'users' (
   'u_id' int(11) NOT NULL AUTO_INCREMENT,
   'username' varchar(255) NOT NULL,
   'password' varchar(255) NOT NULL,
-  'email' varchar(255) NOT NULL,
-  'contact_id' int(11) NOT NULL, FOREIGN KEY (contact_id) REFERENCES contacts(c_id),
-  'lastSeen' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  'lastSeen' varchar(255) NOT NULL,
   PRIMARY KEY ('id')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS 'contacts' (
   'c_id' int(11) NOT NULL AUTO_INCREMENT,
@@ -27,18 +25,17 @@ CREATE TABLE IF NOT EXISTS 'contacts' (
   'email' varchar(255) NOT NULL,
   'phone' varchar(255) NOT NULL,
   'f_name' varchar(255) NOT NULL,
-  's_name' varchar(255) NOT NULL,
-  'lastSeen' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  's_name' varchar(255) NOT NULL,,
   PRIMARY KEY ('id')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS 'meet' (
   'm_id' int(11) NOT NULL AUTO_INCREMENT,
-  'u_id' int(11) NOT NULL, FOREIGN KEY (u_id) REFERENCES users(u_id),
-  'c_id' int(11) NOT NULL, FOREIGN KEY (c_id) REFERENCES contacts(c_id),
+  'user_id' int(11) NOT NULL, FOREIGN KEY (u_id) REFERENCES users(u_id),
+  'contact_id' int(11) NOT NULL, FOREIGN KEY (c_id) REFERENCES contacts(c_id),
   'date' date NOT NULL,
   'time' time NOT NULL,
   'location' varchar(255) NOT NULL,
   'lastSeen' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY ('id')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) DEFAULT CHARSET=latin1;
