b16d7289567f700004a81745e87bb73021030106
Leoš Gjumija <gjumle@protonmail.com>
2022-12-03 09:40:37 +0100

    DM localStorage

--- js/mode.js
+++ js/mode.js
@@ -1,10 +1,28 @@
-var mode = document.getElementById("mode");
-
+/*
 mode.onclick = function() {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains('dark-theme')) {
-        mode.innerHTML = "Light Mode";
+       
     } else {
         mode.innerHTML = "Dark Mode";
     }
 }
+*/
+
+var mode = document.getElementById("mode");
+const btn = document.getElementById('mode');
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-theme");
+    let theme = "light";
+    if (document.body.classList.contains("dark-theme")) {
+        mode.innerHTML = "Light Mode";
+        theme = "dark";
+    }
+    localStorage.setItem("theme", theme);
+});
