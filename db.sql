6e0c7fd69f03999ce7d126338bc70d240dd181db
Leoš Gjumija <gjumle@protonmail.com>
2023-01-04 10:56:52 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -14,3 +14,13 @@ CREATE TABLE users (
     admin INT(1) NOT NULL DEFAULT 0,
     city VARCHAR(50) NOT NULL
 ) DEFAULT CHARSET='latin1';
+
+CREATE TABLE meetings (
+    m_id INT NOT NULL AUTO_INCREMENT,
+    user_id_a INT NOT NULL,
+    user_id_b INT NOT NULL,
+    place VARCHAR(50) NOT NULL,
+    datetime DATETIME NOT NULL,
+    duration TIME NOT NULL,
+    
+)
