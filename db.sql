4df5f3b4a4212b0427ccb1a80cd2a57aecbb8c33
Leoš Gjumija <gjumle@protonmail.com>
2023-01-04 11:47:24 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,13 +1,13 @@
-"C:\Program Files (x86)\EasyPHP-DevServer-14.1VC9\binaries\mysql\bin\mysql.exe"
+"C:\Program Files (x86)\EasyPHP-DevServer-14.1VC9\binaries\mysql\bin\mysql.exe" -u lsa -p
 
-CREATE DATABASE last_seen DEFAULT CHARSET='latin1';
+CREATE DATABASE ls DEFAULT CHARSET='utf8';
 
-CREATE USER last_seen_admin IDENTIFIED BY 'lsa';
+CREATE USER 'lsa'@'localhost' IDENTIFIED BY 'lsa';
 
-GRANT ALL ON last_seen.* TO  'last_seen_admin'@'localhost';
+GRANT ALL ON ls.* TO 'lsa'@'localhost';
 
 CREATE TABLE users (
-    u_id INT NOT NULL AUTO_INCREMENT,
+    u_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE users (
 ) DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE meetings (
-    m_id INT NOT NULL AUTO_INCREMENT,
+    m_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_id_a INT NOT NULL,
     user_id_b INT NOT NULL,
     place VARCHAR(50) NOT NULL,
