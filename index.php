80dfb758ea53755b4774b0e3a4729d35299f7df3
Leoš Gjumija <gjumle@protonmail.com>
2023-01-04 11:51:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 
+function autoloadModel($className) {
+    $filename = "class/" . $className . ".php";
+    if (is_readable($filename)) {
+        require $filename;
+    }
+}
+spl_autoload_register("autoloadModel");
+
+echo $conn::DB;
