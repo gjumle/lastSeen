cd4e48ca5547b555c292c945e9dc9af2594b6edd
Leoš Gjumija <gjumle@protonmail.com>
2023-01-04 11:33:14 +0100

    Update db.php

--- modules/db.php
+++ modules/db.php
@@ -1,5 +1,16 @@
 <?php
 
 class DB {
-    
+    private $host = 'localhost';
+    private $user = 'last_seen_admin';
+    private $pswd = 'lsa';
+    private $db = 'last_seen';
+
+    private static function connect() {
+        $conn = new mysqli($host, $user, $pswd, $db);
+        if (!$conn) {
+            die ('Connection failed: ' . $conn->connect_error);
+        }
+        echo 'Connected successfully';
+    }
 }
