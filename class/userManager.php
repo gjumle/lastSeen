fbb0214e54ea6e05c981d93512f98ff1dc4bb674
Leoš Gjumija <gjumle@protonmail.com>
2023-01-05 11:10:20 +0100

    Update userManager.php

--- class/userManager.php
+++ class/userManager.php
@@ -1,13 +1,5 @@
 <?php
 
-function autoloadModel($className) {
-    $filename = "class/" . $className . ".php";
-    if (is_readable($filename)) {
-        require $filename;
-    }
-}
-spl_autoload_register("autoloadModel");
-
 class UserManager {
     public static function getUsers($uid = null) {
         // Function to load users from DB
