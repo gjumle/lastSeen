52942068e04761a0be59e8cae9722e094d0f6b09
Leoš Gjumija <gjumle@protonmail.com>
2023-01-05 11:30:01 +0100

    Update navbar.php

--- class/navbar.php
+++ class/navbar.php
@@ -2,19 +2,22 @@
 
 class NavBar {
     protected static $navLinks = array();
-    
+  
     public static function addLink($text, $url) {
-        self::$navLinks[] = array('text' => $text, 'url' => $url);
+      self::$navLinks[] = array('text' => $text, 'url' => $url);
     }
-    
+  
     public static function render() {
-        echo '<ul>';
-        foreach(self::$navLinks as $link) {
-        echo "<li><a href='{$link['url']}'>{$link['text']}</a></li>";
-        }
-        echo '</ul>';
-    } 
-}
+      $html = '<div class="nav-bar">';
+      $html .= '<ul>';
+      foreach(self::$navLinks as $link) {
+        $html .= "<li><a href='{$link['url']}'>{$link['text']}</a></li>";
+      }
+      $html .= '</ul>';
+      $html .= '</div>';
+      return $html;
+    }
+  }  
 
 NavBar::addLink('Login', '/login');
 NavBar::addLink('Register', '/register');
