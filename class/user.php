aa52c9c315d3f636f8bbbb312f09f7b6413989a3
Leoš Gjumija <gjumle@protonmail.com>
2023-01-16 20:10:13 +0100

    Update user.php

--- class/user.php
+++ class/user.php
@@ -10,7 +10,7 @@ class User {
 
     public function __construct($uid = null, $username = null, $password = null, $email = null, $admin = null, $city = null) {
         $this->uid = $uid;
-        $this->usermame = $username;
+        $this->username = $username;
         $this->password = $password;
         $this->email = $email;
         $this->admin = $admin;
