0b6a91c860a6ce61dc0c869c5de83613f97239f4
Leoš Gjumija <gjumle@protonmail.com>
2023-01-29 21:06:29 +0100

    Update db.php

--- class/db.php
+++ class/db.php
@@ -6,6 +6,9 @@ class DB {
     public static function connect() {
         if (self::$conn == null) {
             $db = new mysqli('localhost', 'lsa', 'lsa', 'ls');
+            if ($db->connect_error) {
+                die("Connection failed: " . $db->connect_error);
+            }
             self::$conn = $db;
         } else {
             $db = self::$conn;
