c40e2c41df60e8789cd8263833cd6e32e2f760cb
Leoš Gjumija <gjumle@protonmail.com>
2023-02-06 09:45:48 +0100

    Revert "hihihaha"
    This reverts commit 9383f43be72603f6b9e24c44abc8792e9705a26a.

---
+++ class/account.php
@@ -0,0 +1,69 @@
+<?php
+class Account {
+
+    public static function renderForm() {
+        if (isset($_GET['action']) && $_GET['action'] == "register") {
+            return "
+            <div class='account-form'>
+                <form action='' method='post'>
+                    <label for='name'>Name:</label>
+                    <input type='text' name='name'>
+                    
+                    <label for='password'>Password:</label>
+                    <input type='password' name='password'>
+                    
+                    <label for='email'>E-mail:</label>
+                    <input type='email' name='email'>
+                    
+                    <label for='city'>City:</label>
+                    <input type='text' name='city'>
+
+                    <input type='submit' name='register'>
+                </form>
+            </div>";
+        } elseif (isset($_GET['action']) && $_GET['action'] == 'login') {
+            return "
+            <div class='account-form'>
+                <form action='' method='post'>
+                    <label for='name'>Name:</label>
+                    <input type='text' name='name'>
+
+                    <label for='password'>Password:</label>
+                    <input type='password' name='password'>
+
+                    <input type='submit' name='login'>
+                </form>
+            </div>";
+        }
+    }
+
+    public static function formHandler() {
+        $password = (isset($_POST['password'])) ? md5($_POST['password']) : "";
+        if (isset($_POST['register'])) {
+            $registerUser = new User (null, $_POST['name'], $password, null, $_POST['email'], $_POST['city']);
+            $registerUser->insertToDB();
+            echo "<script type='text/javascript'>window.location.replace('account.php');</script>";
+        }
+        if (isset($_POST['login'])) {
+            $loginUser = new User (null, $_POST['name'], $password, null, null, null);
+            if ($loginUser->checkDB() > 0) {
+                $loginUser = UserManager::getUser($loginUser->checkDB());
+                setcookie('uid', $loginUser->getId(), time() + (86400 * 30));
+                setcookie('password', $loginUser->getPassword(), time() + (86400 * 30));
+                setcookie('admin', $loginUser->getAdmin(), time() + (86400 * 30));
+                setcookie('email', $loginUser->getEmail(), time() + (86400 * 30));
+                setcookie('city', $loginUser->getCity(), time() + (86400 * 30));
+                echo "<script type='text/javascript'>window.location.replace('account.php?logedin=1');</script>";
+            } else {
+                return "<span>Inccorect name or password";
+            }
+        }
+    }
+
+    public static function renderLoginForm() {
+        self::formHandler();
+        $form = self::renderForm();
+
+        return $form;
+    }
+}
---
+++ logout.php
@@ -0,0 +1,25 @@
+<?php
+function autoloadModel($className) {
+    $filename = "class/" . $className . ".php";
+    if (is_readable($filename)) {
+        require $filename;
+    }
+}
+spl_autoload_register("autoloadModel");
+
+
+// Delete cookies
+setcookie('uid', '', time() - 3600);
+setcookie('password', '', time() - 3600);
+setcookie('admin', '', time() - 3600);
+setcookie('email', '', time() - 3600);
+setcookie('city', '', time() - 3600);
+header("Refresh: 5; url=index.php");
+?>
+
+<html>
+    <body>
+        <h1>Logout</h1>
+        <p>Logout successfull</p>
+    </body>
+</html>
