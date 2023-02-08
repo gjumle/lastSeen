18fc7f41333336da56c9e3215e1b7e95e38efa71
Leoš Gjumija <gjumle@protonmail.com>
2023-02-08 10:35:19 +0100

    Revert "0"
    This reverts commit 2f4784ce786fc36abcd1bd32ea25b5a8f2ead2b3.

---
+++ js/master.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    $(".registration-form form input").on("focusout", function() {
+        if($(this).val() === "") {
+            $(this).next(".error-message").html("This field is required").css("display", "block");
+        } else {
+            $(this).next(".error-message").html("").css("display", "none");
+        }
+    });
+
+    $(".registration-form form").on("register", function(e){
+        e.preventDefault();
+
+        var isValid = true;
+        $(".registration-form form input").each(function() {
+            if($(this).val() === "") {
+                isValid = false;
+                $(this).next(".error-message").html("This field is required").css("display", "block");
+            }
+        });
+
+        if(isValid) {
+            var data = {
+                "username": $("#username").val(),
+                "password": $("#password").val(),
+                "email": $("#email").val(),
+                "city": $("#city").val()
+            };
+            $.ajax({
+                type: "POST",
+                url: "./register.php",
+                data: data,
+                success: function(response) {
+                    if(response.status === "success") {
+                        alert("Registration successful!");
+                    } else {
+                        alert("Registration failed: " + response.message);
+                    }
+                }
+            });
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('.login-form form input').on('focusout', function() {
+        if($(thi`s`).val() === '') {
+            $(this).next('.error-message').html('This field is required').css('display', 'block');
+        } else {
+            $(this).next('.error-message').html('').css('display', 'none');
+        }
+    });
+
+    $('.login-form form').on('login', function(e) {
+        e.preventDefault();
+
+        var isValid = true;
+        $('.login-form form input').each(function() {
+            if($(this).val() === '') {
+                isValid = false;
+                $(this).next('.error-message').html('This field is required').css('display', 'block');
+            }
+        });
+
+        if(isValid) {
+            var data = {
+                'username': $('#username').val(),
+                'password': $('#password').val()
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: './login.php',
+                data: data,
+                success: function(response) {
+                    if(response.status === 'success') {
+                        alert('Login successful!');
+                    } else {
+                        alert('Login failed: ' + response.message);
+                    }
+                }
+            });
+        }
+    });
+});
+
+
