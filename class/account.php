37e2c9d6c5872106d5e756ffd9fec9dbcc4e1bf4
Leoš Gjumija <gjumle@protonmail.com>
2023-02-08 13:36:39 +0100

    form validation for logjn and register

--- class/account.php
+++ class/account.php
@@ -14,31 +14,31 @@ class Account {
             return "
             <div class='account-form'>
                 <form action='' method='post'>
-                    <div class='form-field>
+                    <div class='form-field'>
                         <label for='name'>Name:</label>
                         <input type='text' name='name'>
                         <span class='error'>*" . self::$nameErr . "</span>
                     <div>
                     
-                    <div class='form-field>
+                    <div class='form-field'>
                         <label for='password'>Password:</label>
                         <input type='password' name='password'>
                         <span class='error'>*" . self::$passwordErr . "</span>
                     <div>
                     
-                    <div class='form-field>
+                    <div class='form-field'>
                         <label for='email'>E-mail:</label>
                         <input type='email' name='email'>
                         <span class='error'>*" . self::$emailErr . "</span>
                     <div>
                     
-                    <div class='form-field>
+                    <div class='form-field'>
                         <label for='city'>City:</label>
                         <input type='text' name='city'>
                         <span class='error'>*" . self::$cityErr . "</span>
                     <div>
 
-                    <div class='form-field>
+                    <div class='form-field'>
                         <input type='submit' name='register'>
                     <div>
                 </form>
@@ -47,63 +47,91 @@ class Account {
             return "
             <div class='account-form'>
                 <form action='" . htmlspecialchars($_SERVER['PHP_SELF']) . "' method='post'>
-                    <label for='name'>Name:</label>
-                    <input type='text' name='name'>
+                    <div class='form-field'>
+                        <label for='name'>Name:</label>
+                        <input type='text' name='name'>
+                        <span class='error'>*" . self::$nameErr . "</span>
+                    </div>
 
-                    <label for='password'>Password:</label>
-                    <input type='password' name='password'>
+                    <div class='form-field'>
+                        <label for='password'>Password:</label>
+                        <input type='password' name='password'>
+                        <span class='error'>*" . self::$passwordErr . "</span>
+                    </div>
 
-                    <input type='submit' name='login'>
+                    <div class='form-field'>
+                        <input type='submit' name='login'>
+                    </div>
                 </form>
             </div>";
         }
     }
 
-    public static function formHandler() {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    public static function requireFields($formType) {
+        // Checks if the POSTs are filled up
+        // Checks the POSTs based on form type 
+        if ($formType == 'login') {
             if (empty($_POST['name'])) {
                 self::$nameErr = 'Name is required';
             } else {
                 self::$name = trim($_POST['name']);
             }
-
             if (empty($_POST['password'])) {
                 self::$passwordErr = 'Password is required';
             } else {
                 self::$password = md5($_POST['password']);
             }
-
+        } elseif ($formType == 'register') {
+            if (empty($_POST['name'])) {
+                self::$nameErr = 'Name is required';
+            } else {
+                self::$name = trim($_POST['name']);
+            }
+            if (empty($_POST['password'])) {
+                self::$passwordErr = 'Password is required';
+            } else {
+                self::$password = md5($_POST['password']);
+            }
             if (empty($_POST['email'])) {
                 self::$emailErr = 'Email is required';
             } else {
-                self::$email = $_POST['email'];
+                self::$email = trim($_POST['email']);
             }
-
             if (empty($_POST['city'])) {
                 self::$cityErr = 'City is required';
             } else {
-                self::$city = $_POST['city'];
+                self::$city = trim($_POST['city']);
             }
+        }
+    }
 
-        }
-        if (isset($_POST['register'])) {
-            $registerUser = new User (null, self::$name, self::$password, null, self::$email, self::$city);
-            $registerUser->insertToDB();
-        }
-        if (isset($_POST['login'])) {
-            $loginUser = new User (null, self::$name, self::$password, null, null, null);
-            if ($loginUser->checkDB() > 0) {
-                $loginUser = UserManager::getUser($loginUser->checkDB());
-                setcookie('logged_in', true, time() + (86400 * 30));
-                setcookie('uid', $loginUser->getId(), time() + (86400 * 30));
-                setcookie('name', $loginUser->getName(), time() + (86400 * 30));
-                setcookie('password', $loginUser->getPassword(), time() + (86400 * 30));
-                setcookie('admin', $loginUser->getAdmin(), time() + (86400 * 30));
-                setcookie('email', $loginUser->getEmail(), time() + (86400 * 30));
-                setcookie('city', $loginUser->getCity(), time() + (86400 * 30));
-                echo "<script type='text/javascript'>window.location.replace('userAccount.php');</script>";
-            } else {
-                return "<span>Inccorect name or password";
+    public static function formHandler() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (isset($_POST['register'])) {
+                // Form validation
+                Account::requireFields('register');
+                $registerUser = new User (null, self::$name, self::$password, null, self::$email, self::$city);
+                // Save user to DB
+                $registerUser->insertToDB();
+            }
+            if (isset($_POST['login'])) {
+                // Form validation
+                Account::requireFields('login');
+                $loginUser = new User (null, self::$name, self::$password, null, null, null);
+                // Compare name and password from DB with the POST data
+                if ($loginUser->checkDB() > 0) {
+                    $loginUser = UserManager::getUser($loginUser->checkDB());
+                    setcookie('logged_in', true, time() + (86400 * 30));
+                    setcookie('uid', $loginUser->getId(), time() + (86400 * 30));
+                    setcookie('name', $loginUser->getName(), time() + (86400 * 30));
+                    setcookie('password', $loginUser->getPassword(), time() + (86400 * 30));
+                    setcookie('admin', $loginUser->getAdmin(), time() + (86400 * 30));
+                    setcookie('email', $loginUser->getEmail(), time() + (86400 * 30));
+                    setcookie('city', $loginUser->getCity(), time() + (86400 * 30));
+                    echo "<script type='text/javascript'>window.location.replace('userAccount.php');</script>";
+                } else {
+                    return "<span>Inccorect name or password";
+                }
             }
         }
     }
