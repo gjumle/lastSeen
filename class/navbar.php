658e65446bac866d2617c3e5a45e4ccb4213cb7a
Leoš Gjumija <gjumle@protonmail.com>
2023-02-08 09:32:08 +0100

    Update navbar.php

--- class/navbar.php
+++ class/navbar.php
@@ -13,20 +13,30 @@ class NavBar {
         foreach(self::$links as $name => $url) {
             $html .= '<li><a href="'.$url.'"'.($current_page == $url ? ' class="active"' : '').'>'.$name.'</a></li>';
         }
-        $html .= '<li class="dropdown" style="float:right">';
-        $html .= '<a href="#">Account</a>';
-        $html .= '<div class="dropdown-content">';
         if(isset($_COOKIE['logged_in']) && $_COOKIE['logged_in'] == true) {
+            $html .= '<li><a href="./meetings.php">Meetings</a></li>';
+            if (isset($_COOKIE['admin']) && $_COOKIE['admin'] == true) {
+                $html .= '<li><a href="./meetingmanager.php">Meeting Manager</a></li>';
+                $html .= '<li><a href="./usermanager.php">User Manager</a></li>';
+            }
+            $html .= '<li class="dropdown" style="float:right">';
+            $html .= '<a href="#">Account</a>';
+            $html .= '<div class="dropdown-content">';
             if(isset($_COOKIE['username'])) {
                 $html .= '<a href="./account.php?username='.$_COOKIE['username'].'">'.$_COOKIE['username'].'</a>';
             }            
             $html .= '<a href="./logout.php">Logout</a>';
+            $html .= '</div>';
+            $html .= '</li>';
         } else {
+            $html .= '<li class="dropdown" style="float:right">';
+            $html .= '<a href="#">Account</a>';
+            $html .= '<div class="dropdown-content">';
             $html .= '<a href="./login.php">Login</a>';
             $html .= '<a href="./register.php">Register</a>';
+            $html .= '</div>';
+            $html .= '</li>';
         }
-        $html .= '</div>';
-        $html .= '</li>';
         $html .= '</ul>';
         $html .= '</nav>';
         return $html;
