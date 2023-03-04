f6e1185cb197842847990eb3dadd2adc4be3d194
Leoš Gjumija <gjumle@protonmail.com>
2023-03-04 19:48:52 +0100

    mimii

---
+++ dashboard.php
@@ -0,0 +1,10 @@
+<?php
+function autoloadModel($className) {
+    $filename = "class/" . $className . ".php";
+    if (is_readable($filename)) {
+        require $filename;
+    }
+}
+spl_autoload_register("autoloadModel");
+
+Render::renderDashboardPage();
