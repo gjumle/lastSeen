262f4fb209136a566477d6b5eb9b86406983cfdf
Leoš Gjumija <gjumle@protonmail.com>
2023-03-08 12:49:00 +0100

    navbar

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Last Seen</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta lang="en">
+
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/master.css">
+</head>
+<body>
+<nav>
+    <a href="#" class="logo"><img src="./img/logo.png" alt="Logo"></a>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li class="dropdown">
+            <a href="#" class="dropbtn">Account</a>
+            <div class="dropdown-content">
+                <a href="#">Login</a>
+                <a href="#">Register</a>
+            </div>
+        </li>
+    </ul>
+</nav>
+
+    
+</body>
+</html>
