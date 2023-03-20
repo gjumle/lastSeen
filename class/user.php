ea1e3a8f5735265952417f4208f7eca5a971479a
Leoš Gjumija <gjumle@protonmail.com>
2023-03-20 21:16:58 +0100

    Update user.php

--- class/user.php
+++ class/user.php
@@ -349,7 +349,9 @@ class User {
                                 </form>
                             </div>
                         </main>
-                    </div>';
+                    </div>
+                </div>
+            </div>';
         }
         $users = User::getUsers($uid);
         foreach ($users as $user) {
@@ -450,7 +452,10 @@ class User {
                                     </form>
                                 </div>
                             </main>
-                        </div>';
+                        </div>
+                    </div>
+                </div>';
+                
             } else {
                 echo
                 '<div id="dashboard-feed" class="main col-lg-6 col-md-8">
@@ -565,7 +570,9 @@ class User {
                                     </div>
                                 </div>
                             </main>
-                        </div>';
+                        </div>
+                    </div>
+                </div>';                
             }
         }
     }
@@ -575,99 +582,99 @@ class User {
         if (isset($_GET['edit']) && $_GET['edit'] == $user->getUid()) {
             echo
             '<div class="content">
-                        <main id="main" class="feed-mfe">
-                            <div class="package-feed-ui">
-                                <form method="post" action="">
-                                <div class="feed-ui-components">
-                                    <div class="feed-ui-header">
-                                        <div class="feed-ui-media">
-                                            <div class="feed-ui-media-left">
-                                                <div class="feed-ui-icon-container">
-                                                    <a href="#" class="ui-avatar">
-                                                        <div class="ui-img-wrapper">
-                                                            <img src="./svg/avatar.svg" alt="avatar">
-                                                        </div>
-                                                    </a>
+                <main id="main" class="feed-mfe">
+                    <div class="package-feed-ui">
+                        <form method="post" action="">
+                        <div class="feed-ui-components">
+                            <div class="feed-ui-header">
+                                <div class="feed-ui-media">
+                                    <div class="feed-ui-media-left">
+                                        <div class="feed-ui-icon-container">
+                                            <a href="#" class="ui-avatar">
+                                                <div class="ui-img-wrapper">
+                                                    <img src="./svg/avatar.svg" alt="avatar">
                                                 </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="feed-ui-media-body">
+                                        <div class="feed-ui-media-body-header">
+                                            <a href="./profile">' . $_COOKIE["f_name"], ' ' . $_COOKIE["l_name"] . '</a>
+                                        </div>
+                                        <div class="feed-ui-media-body-subtitle-wrapper">
+                                            <time class="timestamp text-medium" datetime="2023-02-27 00-13-30 UTC">' . User::getDateAsString($user->getTimestamp()) . '</time>
+                                        </div>
+                                    </div>
+                                    <div class="feed-ui-media-right">
+                                        <div class="feed-ui-media-right-components">
+                                            <div class="feed-ui-media-right-component">
+                                                <input type="submit" class="btn btn-primary btn-delete" type="submit" name="save" value="Save">
                                             </div>
-                                            <div class="feed-ui-media-body">
-                                                <div class="feed-ui-media-body-header">
-                                                    <a href="./profile">' . $_COOKIE["f_name"], ' ' . $_COOKIE["l_name"] . '</a>
-                                                </div>
-                                                <div class="feed-ui-media-body-subtitle-wrapper">
-                                                    <time class="timestamp text-medium" datetime="2023-02-27 00-13-30 UTC">' . User::getDateAsString($user->getTimestamp()) . '</time>
-                                                </div>
-                                            </div>
-                                            <div class="feed-ui-media-right">
-                                                <div class="feed-ui-media-right-components">
-                                                    <div class="feed-ui-media-right-component">
-                                                        <input type="submit" class="btn btn-primary btn-delete" type="submit" name="save" value="Save">
-                                                    </div>
-                                                    <div class="feed-ui-media-right-component">
-                                                        <a href="?cancel=' . $user->getUid() . '" class="btn btn-primary btn-edit" type="submit" name="cancel">Cancel</a>
-                                                    </div>
-                                                </div>
+                                            <div class="feed-ui-media-right-component">
+                                                <a href="?cancel=' . $user->getUid() . '" class="btn btn-primary btn-edit" type="submit" name="cancel">Cancel</a>
                                             </div>
                                         </div>
                                     </div>
-                                    <div id="feed-body" class="feed-ui-body">
-                                        <div class="feed-ui-media">
-                                            <div class="feed-ui-media-left">
-                                                <div class="feed-ui-icon-activity">
-                                                    <svg class="icon-activity">
-                                                        <path d="M19.99 13.33a3.7 3.7 0 01-3.32-2l-.17-.32h-1.01l-.17.32a3.763 3.763 0 01-6.65 0l-.17-.32H7.49l-.17.32a3.72 3.72 0 01-3.32 2 3.7 3.7 0 01-3.01-1.51v1.88a5.02 5.02 0 003.01.98 5.054 5.054 0 004-1.92 5.116 5.116 0 007.99 0 5.122 5.122 0 007.01.94v-1.88a3.71 3.71 0 01-3.01 1.51zm-7.99 8a3.725 3.725 0 01-3.33-2L8.49 19H7.5l-.18.33a3.7 3.7 0 01-3.32 2 3.7 3.7 0 01-3.01-1.51v1.89c.873.64 1.929.98 3.01.97a5.054 5.054 0 004-1.92 5.054 5.054 0 004 1.92 4.947 4.947 0 003-.98v-1.87a3.654 3.654 0 01-3 1.5zm8-16.02a3.735 3.735 0 01-3.33-2L16.51 3h-1.02l-.16.31a3.724 3.724 0 01-3.33 2 3.681 3.681 0 01-3-1.5V5.7a5.04 5.04 0 003 .96 5.024 5.024 0 004-1.92 5.023 5.023 0 004 1.92 5.124 5.124 0 003-.95v-1.9a3.654 3.654 0 01-3 1.5z" fill=""></path>
-                                                    </svg>
-                                                </div>
-                                            </div>
-                                            <div class="feed-ui-media-body">
-                                                <div class="feed-ui-media-body-activity">
-                                                    <h3 class="feed-body-header"><input type="text" name="f_name" value="' . $user->getF_Name(), '"> <input type="text" name="l_name" value="' . $user->getL_Name() . '"></h3>
-                                                    <div class="feed-ui-media">
-                                                        <div class="feed-ui-nmedia-body">
-                                                            <ul class="feed-media-items">
-                                                                <li class="feed-media-item">
-                                                                    <div class="package-stat">
-                                                                        <span class="stat-label">
-                                                                            Type
-                                                                        </span>
-                                                                        <div class="stat-value">
-                                                                            <select name="status">';
-                                                                            if ($user->getAdmin() == 1) {
-                                                                                echo '<option value="1" selected>Admin</option>';
-                                                                            } elseif ($_COOKIE['admin'] == 1) {
-                                                                                echo '<option value="1">Admin</option>';
-                                                                            }
-                                                                            if ($user->getAdmin() == 0) {
-                                                                                echo '<option value="0" selected>User</option>';
-                                                                            } else {
-                                                                                echo '<option value="0">User</option>';
-                                                                            }
-                                                                            echo '</select>
-                                                                        </div>
-                                                                    </div>
-                                                                </li>
-                                                                <li class="feed-media-item">
-                                                                    <div class="package-stat">
-                                                                        <span class="stat-label">
-                                                                            E-mail                 
-                                                                        </span>
-                                                                        <div class="stat-value">
-                                                                            <input type="email" name="email" value="' . $user->getEmail() . '">
-                                                                        </div>
-                                                                    </div>
-                                                                </li>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
+                                </div>
+                            </div>
+                            <div id="feed-body" class="feed-ui-body">
+                                <div class="feed-ui-media">
+                                    <div class="feed-ui-media-left">
+                                        <div class="feed-ui-icon-activity">
+                                            <svg class="icon-activity">
+                                                <path d="M19.99 13.33a3.7 3.7 0 01-3.32-2l-.17-.32h-1.01l-.17.32a3.763 3.763 0 01-6.65 0l-.17-.32H7.49l-.17.32a3.72 3.72 0 01-3.32 2 3.7 3.7 0 01-3.01-1.51v1.88a5.02 5.02 0 003.01.98 5.054 5.054 0 004-1.92 5.116 5.116 0 007.99 0 5.122 5.122 0 007.01.94v-1.88a3.71 3.71 0 01-3.01 1.51zm-7.99 8a3.725 3.725 0 01-3.33-2L8.49 19H7.5l-.18.33a3.7 3.7 0 01-3.32 2 3.7 3.7 0 01-3.01-1.51v1.89c.873.64 1.929.98 3.01.97a5.054 5.054 0 004-1.92 5.054 5.054 0 004 1.92 4.947 4.947 0 003-.98v-1.87a3.654 3.654 0 01-3 1.5zm8-16.02a3.735 3.735 0 01-3.33-2L16.51 3h-1.02l-.16.31a3.724 3.724 0 01-3.33 2 3.681 3.681 0 01-3-1.5V5.7a5.04 5.04 0 003 .96 5.024 5.024 0 004-1.92 5.023 5.023 0 004 1.92 5.124 5.124 0 003-.95v-1.9a3.654 3.654 0 01-3 1.5z" fill=""></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div class="feed-ui-media-body">
+                                        <div class="feed-ui-media-body-activity">
+                                            <h3 class="feed-body-header"><input type="text" name="f_name" value="' . $user->getF_Name(), '"> <input type="text" name="l_name" value="' . $user->getL_Name() . '"></h3>
+                                            <div class="feed-ui-media">
+                                                <div class="feed-ui-nmedia-body">
+                                                    <ul class="feed-media-items">
+                                                        <li class="feed-media-item">
+                                                            <div class="package-stat">
+                                                                <span class="stat-label">
+                                                                    Type
+                                                                </span>
+                                                                <div class="stat-value">
+                                                                    <select name="status">';
+                                                                    if ($user->getAdmin() == 1) {
+                                                                        echo '<option value="1" selected>Admin</option>';
+                                                                    } elseif ($_COOKIE['admin'] == 1) {
+                                                                        echo '<option value="1">Admin</option>';
+                                                                    }
+                                                                    if ($user->getAdmin() == 0) {
+                                                                        echo '<option value="0" selected>User</option>';
+                                                                    } else {
+                                                                        echo '<option value="0">User</option>';
+                                                                    }
+                                                                    echo '</select>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li class="feed-media-item">
+                                                            <div class="package-stat">
+                                                                <span class="stat-label">
+                                                                    E-mail                 
+                                                                </span>
+                                                                <div class="stat-value">
+                                                                    <input type="email" name="email" value="' . $user->getEmail() . '">
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                </form>
                             </div>
-                        </main>
-                    </div>';
+                        </div>
+                        </form>
+                    </div>
+                </main>
+            </div>';
         } else {
             echo
             '<div class="content">
