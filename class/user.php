e0d365f63c504ecc19f99165dda1c01ac30277a7
Leoš Gjumija <gjumle@protonmail.com>
2023-03-20 12:11:27 +0100

    Update user.php

--- class/user.php
+++ class/user.php
@@ -171,6 +171,9 @@ class User {
                 $user->setUsername($_POST['username']);
                 $user->setF_name($_POST['f_name']);
                 $user->setL_name($_POST['l_name']);
+                if (isset($_POST['admin']) && isset($_COOKIE['admin']) && $_COOKIE['admin'] == 1) {
+                    $user->setAdmin($_POST['status']);
+                }
                 $user->setPassword($_POST['password']);
                 $user->setAdmin($_POST['admin']);
                 $user->setEmail($_POST['email']);
